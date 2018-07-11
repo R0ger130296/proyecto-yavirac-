6226cd15f9936c31076f8c8d20e18a892d30981c
andy cedeño <grarogccee@gmail.com>
2018-07-10 23:10:58 -0500

    chart.js

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +10,6 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  
 
 }
