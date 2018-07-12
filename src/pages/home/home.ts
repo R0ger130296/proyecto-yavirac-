5605e97c262c8ffe4cc181d6b719aebb91a78377
andy cedeño <grarogccee@gmail.com>
2018-07-12 18:01:18 -0500

    borre el chart malo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-importar { Chart } desde  ' chart.js ' ;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
