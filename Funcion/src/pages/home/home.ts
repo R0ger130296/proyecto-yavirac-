f8a3d62b8c9f93879d7ba44ac32d70a5267cd3fc
Omar Cedeño <grarogccee@gmail.com>
2018-07-29 16:52:59 -0500

    probando

--- Funcion/src/pages/home/home.ts
+++ Funcion/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {chart} from 'chart.js';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
