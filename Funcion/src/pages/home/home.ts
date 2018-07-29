06d341567fc3d67901e16a0103c3e1cc14a9b29a
Omar Cedeño <grarogccee@gmail.com>
2018-07-29 17:02:18 -0500

    funciones de paginas

--- Funcion/src/pages/home/home.ts
+++ Funcion/src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {chart} from 'chart.js';
+import { SenoPage } from '../seno/seno';
+import { CosenoPage } from '../coseno/coseno';
+import { CosecantePage } from '../cosecante/cosecante';
+import { SecantePage } from '../secante/secante';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +14,16 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+SENO():void{
+this.navCtrl.push(SenoPage);
+}
+COSENO():void{
+  this.navCtrl.push(CosenoPage);
+}
+COSECANTE():void{
+  this.navCtrl.push(CosecantePage);
+}
+SECANTE():void{
+  this.navCtrl.push(SecantePage);
+}
 }
